f4a3238f44b8205f7897742480e355f7e891dd29
lucasfelipeterra <lucasterra622@gmail.com>
2024-10-08 17:04:58 -0500

    atualizado

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,10 @@
 import mongoose from "mongoose";
 
-const connectDB = async() => {
+const connectDB = async () => {
     try {
-        await mongoose.connect(
-            `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`
-          );
-
+        await mongoose.connect(`mongodb+srv://${process.env.MONGODB}/${process.env.DB_NAME}`);
     } catch (error) {
-        console.error('Erro ao conectar ao MongoDB', error);
-
+        console.error("Erro ao conectar ao MongoDB", error);
     }
 };
 
